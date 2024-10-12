7ef4cebbbf879d35ba9be77b444d9b3842202286
Aryan Kumar Dubey <96795631+Aryan-we@users.noreply.github.com>
2024-10-12 15:00:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+//https://drive.google.com/drive/folders/1L0wwCdidoEhj30eRQd9_pkY6BZtGNC6J
+function func1(){
+    document.getElementById("s-head").innerText="Video 1";
+    document.getElementById("svideo").src="./short videos/video 1.mp4";
+}
+function func2(){
+    document.getElementById("s-head").innerText="Video 2";
+    document.getElementById("svideo").src="https://drive.google.com/file/d/12DKvqdCPlauiWfQxAFlfANfOa19iT4Cn/view?usp=drive_link";
+}
+function func3(){
+    document.getElementById("s-head").innerText="Video 3";
+    document.getElementById("svideo").src="./short videos/video 3.mp4";
+}
+function func4(){
+    document.getElementById("s-head").innerText="Video 4";
+    document.getElementById("svideo").src="./short videos/video 4.mp4";
+}
+function func5(){
+    document.getElementById("v-head").innerText="Video 1";
+    document.getElementById("lvideo").src="./long videos/video 1.mp4"
+}
+function func6(){
+    document.getElementById("v-head").innerText="Video 2";
+    document.getElementById("lvideo").src="./long videos/video 2.mp4";
+}
+function func7(){
+    document.getElementById("v-head").innerText="Video 3";
+    document.getElementById("lvideo").src="./long videos/video 3.mp4";
+}
+function func8(){
+    document.getElementById("v-head").innerText="Video 4";
+    document.getElementById("lvideo").src="./long videos/video 4.mp4";
+}
+function func9(){
+    document.getElementById("g-head").innerText="Video 1";
+    document.getElementById("gvideo").src="./gaming/Gaming Edited YT Video.mp4";
+}
+
+function func10(){
+    document.getElementById("f-head").innerText="Video 1";
+    document.getElementById("fvideo").src="./football edits/Ronaldo 3.mp4";
+}
+function func11(){
+    document.getElementById("f-head").innerText="Video 2";
+    document.getElementById("fvideo").src="./football edits/Ronaldo vs messi.mp4";
+}
+function func12(){
+    document.getElementById("e-head").innerText="Video 1";
+    document.getElementById("evideo").src="./e-commerce ads/1st Mockup Edit.mp4";
+}
+
+function func13(){
+    document.getElementById("e-head").innerText="Video 2";
+    document.getElementById("evideo").src="./e-commerce ads/2nd Mockup Edit.mp4";
+}
+function func14(){
+    document.getElementById("e-head").innerText="Video 3";
+    document.getElementById("evideo").src="./e-commerce ads/3rd Mockup Edit.mp4";
+}
+function func15(){
+    document.getElementById("a-head").innerText="Video 1";
+    document.getElementById("avideo").src="./anime videos/Comp 1_1.mp4";
+}
+function func16(){
+    document.getElementById("c-head").innerText="Video 1";
+    document.getElementById("cvideo").src="./color grading/Color Grading.mp4";
+}
+function func17(){
+    document.getElementById("d-head").innerText="Video 1";
+    document.getElementById("dvideo").src="./document style/Mohak Mangal Documentry Style.mp4";
+}
+function func18(){
+    document.getElementById("d-head").innerText="Video 2";
+    document.getElementById("dvideo").src="./document style/self improvement.mp4";
+}
+function func19(){
+    document.getElementById("ads-head").innerText="Video 1";
+    document.getElementById("adsvideo").src="./ads/Coca Cola Landing Page AD.mp4";
+}
+function func20(){
+    document.getElementById("ads-head").innerText="Video 2";
+    document.getElementById("adsvideo").src="./ads/Comp 1_1_1.mp4";
+}
+function func21(){
+    document.getElementById("ads-head").innerText="Video 3";
+    document.getElementById("adsvideo").src="./ads/Gta V Game Maza Bank .mp4";
+}
+
